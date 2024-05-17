5a0c0ad347d5f1507f9f49ab287607bfb0d04178
Pacomerlo482611 <francisco.javier.cabello@hotmail.com>
2024-05-17 09:45:12 +0200

    cambios

---
+++ script/parImpar.js
@@ -0,0 +1,22 @@
+let boton=document.querySelector(".boton");
+
+boton.addEventListener("click", function(){
+    let num=document.querySelector("#numero").value;
+    if (num==""){
+
+        document.querySelector(".mensajeError").innerHTML="Debe ingresar un número para poderle indicar si es par o impar."
+    }
+
+
+
+
+
+
+
+
+
+
+
+
+    
+})
