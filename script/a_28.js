0b49d80f1cfe3cea5160aba88c472fce22cfdb98
Pacomerlo482611 <francisco.javier.cabello@hotmail.com>
2024-05-17 09:33:50 +0200

    cambios

--- script/a_28.js
+++ script/a_28.js
@@ -1,80 +1,43 @@
 
 let boton = document.querySelector(".boton");
-
-//necesitamos que cuando pulse clic en el boton, haga una condicion
-//y analice la edad para enviar un mensaje de vuelta...
-
-
-
+//necesitamos que cuando pulse clic en el boton, haga una condición
+//y analice la edad para enviar un mensaje de vuelta
 //javascript los eventos
 
-
 boton.addEventListener("click", function () {
-
     let nombre = document.querySelector("#nombre").value;
     let edad = document.querySelector("#edad").value;
-    console.log(nombre);
-    console.log(edad);
-
     //nos trae el valor que se ha escrito en el formulario
-
     let mensaje;
     if (edad <= 12) {
-
-        mensaje = "niñ@";
+        mensaje = "niñ@"
     } else if (edad >= 13 && edad <= 17) {
-
         mensaje = "adolescente";
     } else if (edad >= 18 && edad <= 64) {
         mensaje = "trabajador/a"
     } else {
         mensaje = "jubilado"
     }
-
-
-
     let msnError = "";
     let error = false;
-    //validacion de datos de entrada
-
+    //validación de datos de entrada
     if (edad < 0 || edad == "") {
-
-        msnError = "La edad no es correcta, ingrese una edad correcta";
+        msnError = "Ingrese una edad correcta";
         document.querySelector("#edad").value="";
-        error = true
-    }
-
-    if (nombre == "") {
-        msnError += "<br>Ingrese un nombre "
+        error = true;
+    } if (nombre == "") {
+        msnError += "<br>Ingrese un nombre";
         error = true;
     }
-
     if (error) {
-        document.querySelector(".mensaje").innerHTML = "";//borra el contenido de mensaje
+        document.querySelector(".mensaje").innerHTML = ""; 
         document.querySelector(".mensajeError").innerHTML = `${msnError}`;
-
     }
-
     else {
-
-        document.querySelector(".mensajeError").innerHTML = "";//borra el contenido de mensajeError
-        document.querySelector(".mensaje").innerHTML = `${nombre} tiene ${edad} años y es ${mensaje}`;
+        document.querySelector(".mensajeError").innerHTML = ""; //borrar contenido
+        document.querySelector(".mensaje").innerHTML = `${nombre} tiene ${edad} años y es ${mensaje}`
     }
-
-
-
-
-
-
-
 })
-
-
-
-
-
-
-
 
 
 
